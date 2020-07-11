ac8e6aca06aded2e6170ba192548904648d0fec3
Devan-Kerman <devan@cleverpath.com>
2020-07-10 21:23:03 -0500

    fix /reload error

--- src/main/java/net/devtech/arrp/mixin/ReloadableResourceManagerImplMixin.java
+++ src/main/java/net/devtech/arrp/mixin/ReloadableResourceManagerImplMixin.java
@@ -1,8 +1,7 @@
 package net.devtech.arrp.mixin;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 
 import net.devtech.arrp.api.RRPCallback;
 import org.apache.logging.log4j.Logger;
@@ -10,22 +9,21 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import net.minecraft.resource.ReloadableResourceManagerImpl;
 import net.minecraft.resource.ResourcePack;
-import net.minecraft.resource.ResourceReloadMonitor;
-import net.minecraft.util.Unit;
 
 @Mixin (ReloadableResourceManagerImpl.class)
 public class ReloadableResourceManagerImplMixin {
 	@Shadow @Final private static Logger LOGGER;
 
-	@Inject (method = "beginMonitoredReload", at = @At ("HEAD"))
-	private void registerARRPs(Executor prepareExecutor, Executor applyExecutor, CompletableFuture<Unit> initialStage, List<ResourcePack> packs, CallbackInfoReturnable<ResourceReloadMonitor> cir) {
+	@ModifyVariable (method = "beginMonitoredReload", at = @At ("HEAD"), argsOnly = true)
+	private List<ResourcePack> registerARRPs(List<ResourcePack> packs) {
+		packs = new ArrayList<>(packs);
 		LOGGER.info("ARRP register");
 		RRPCallback.EVENT.invoker()
 		                 .insert(packs);
+		return packs;
 	}
 }
