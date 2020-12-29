79f62fbf0089ca2d5ec5c2c73b6cc07daa6e18b6
hYdos <haydenv06@gmail.com>
2020-12-30 09:01:03 +1000

    use a nicer logger

--- src/main/java/net/devtech/arrp/ARRP.java
+++ src/main/java/net/devtech/arrp/ARRP.java
@@ -1,19 +1,19 @@
 package net.devtech.arrp;
 
-import java.util.logging.Logger;
-
 import net.devtech.arrp.api.RRPPreGenEntrypoint;
 import net.devtech.arrp.impl.RuntimeResourcePackImpl;
 
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ARRP implements PreLaunchEntrypoint {
-	private static final Logger LOGGER = Logger.getLogger("ARRP");
+	private static final Logger LOGGER = LogManager.getLogger("ARRP");
 
 	@Override
 	public void onPreLaunch() {
-		LOGGER.fine("I used the json to destroy the json");
+		LOGGER.info("I used the json to destroy the json");
 		FabricLoader loader = FabricLoader.getInstance();
 		for (RRPPreGenEntrypoint entrypoint : loader.getEntrypoints("rrp:pregen", RRPPreGenEntrypoint.class)) {
 			RuntimeResourcePackImpl.EXECUTOR_SERVICE.submit(entrypoint::pregen);
