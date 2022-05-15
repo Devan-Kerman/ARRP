288e46f4dbe4ba19b9230fd442be1085ef7f6e4a
Bill Parrott <bill@chimericdream.com>
2022-05-14 22:07:48 -0500

    fix(JVariant): try to fix serialization issue for JVariant (fixes #62)

--- src/main/java/net/devtech/arrp/json/blockstate/JVariant.java
+++ src/main/java/net/devtech/arrp/json/blockstate/JVariant.java
@@ -1,7 +1,9 @@
 package net.devtech.arrp.json.blockstate;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.google.gson.JsonElement;
@@ -13,7 +15,7 @@ import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.math.Direction;
 
 public final class JVariant implements Cloneable {
-	private final Map<String, JBlockModel> models = new HashMap<>();
+	private final Map<String, List<JBlockModel>> models = new HashMap<>();
 
 	/**
 	 * @see JState#variant()
@@ -21,24 +23,27 @@ public final class JVariant implements Cloneable {
 	public JVariant() {}
 
 	public JVariant put(String key, JBlockModel model) {
-		this.models.put(key, model);
+		List<JBlockModel> models = this.models.getOrDefault(key, new ArrayList<>());
+
+		models.add(model);
+
+		this.models.put(key, models);
+
 		return this;
 	}
 
 	/**
 	 * boolean block properties
 	 */
-	public JVariant put(String property, boolean value, JBlockModel builder) {
-		this.models.put(property + '=' + value, builder);
-		return this;
+	public JVariant put(String property, boolean value, JBlockModel model) {
+		return this.put(property + '=' + value, model);
 	}
 
 	/**
 	 * int block properties
 	 */
-	public JVariant put(String property, int value, JBlockModel builder) {
-		this.models.put(property + '=' + value, builder);
-		return this;
+	public JVariant put(String property, int value, JBlockModel model) {
+		return this.put(property + '=' + value, model);
 	}
 
 	/**
@@ -46,17 +51,15 @@ public final class JVariant implements Cloneable {
 	 *
 	 * @see Direction
 	 */
-	public JVariant put(String property, StringIdentifiable value, JBlockModel builder) {
-		this.models.put(property + '=' + value.asString(), builder);
-		return this;
+	public JVariant put(String property, StringIdentifiable value, JBlockModel model) {
+		return this.put(property + '=' + value.asString(), model);
 	}
 
 	/**
 	 * everything else
 	 */
-	public JVariant put(String property, String value, JBlockModel builder) {
-		this.models.put(property + '=' + value, builder);
-		return this;
+	public JVariant put(String property, String value, JBlockModel model) {
+		return this.put(property + '=' + value, model);
 	}
 
 	@Override
