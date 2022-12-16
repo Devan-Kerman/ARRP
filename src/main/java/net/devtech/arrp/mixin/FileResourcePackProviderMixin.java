749af797b9bebdd4e79570152855e5f0d44f7a94
ueaj <dev.sel20@gmail.com>
2022-12-15 20:51:19 -0600

    fix new register system

--- src/main/java/net/devtech/arrp/mixin/FileResourcePackProviderMixin.java
+++ src/main/java/net/devtech/arrp/mixin/FileResourcePackProviderMixin.java
@@ -2,13 +2,14 @@ package net.devtech.arrp.mixin;
 
 import net.devtech.arrp.ARRP;
 import net.devtech.arrp.api.RRPCallback;
-import net.minecraft.resource.FileResourcePackProvider;
-import net.minecraft.resource.ResourcePack;
-import net.minecraft.resource.ResourcePackProfile;
-import net.minecraft.resource.ResourcePackSource;
+import net.minecraft.resource.*;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,15 +17,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 @Mixin(FileResourcePackProvider.class)
 public class FileResourcePackProviderMixin {
-	private static final ResourcePackSource RUNTIME = ResourcePackSource.nameAndSource("pack.source.runtime");
+	@Shadow @Final private ResourceType type;
+	private static final ResourcePackSource RUNTIME = ResourcePackSource.create(getSourceTextSupplier(), true);
 	private static final Logger ARRP_LOGGER = LogManager.getLogger("ARRP/FileResourcePackProviderMixin");
+
+	private static UnaryOperator<Text> getSourceTextSupplier() {
+		Text text = Text.translatable("pack.source.runtime");
+		return name -> Text.translatable("pack.nameAndSource", name, text).formatted(Formatting.GRAY);
+	}
+
 	@Inject(method = "register", at = @At("HEAD"))
 	public void register(
 		Consumer<ResourcePackProfile> adder,
-		ResourcePackProfile.Factory factory,
 		CallbackInfo ci
 	) throws ExecutionException, InterruptedException {
 		List<ResourcePack> list = new ArrayList<>();
@@ -33,11 +41,12 @@ public class FileResourcePackProviderMixin {
 		RRPCallback.BEFORE_USER.invoker().insert(list);
 
 		for (ResourcePack pack : list) {
-			adder.accept(ResourcePackProfile.of(
+			adder.accept(ResourcePackProfile.create(
 				pack.getName(),
+				Text.literal(pack.getName()),
 				false,
-				() -> pack,
-				factory,
+				(name) -> pack,
+				this.type,
 				ResourcePackProfile.InsertionPosition.TOP,
 				RUNTIME
 			));
