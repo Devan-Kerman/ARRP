3a685438960b7fa64adaf71f7f13fba30d9b60f7
ueaj <dev.sel20@gmail.com>
2024-01-08 17:36:19 -0600

    nip potential security issue in the bud: v0.8.1

--- src/main/java/net/devtech/arrp/impl/RuntimeResourcePackImpl.java
+++ src/main/java/net/devtech/arrp/impl/RuntimeResourcePackImpl.java
@@ -301,9 +301,14 @@ public class RuntimeResourcePackImpl implements RuntimeResourcePack, ResourcePac
 		// data dump time
 		try {
 			for(Map.Entry<List<String>, Supplier<byte[]>> e : this.root.entrySet()) {
-				Path root = output.resolve(String.join("/", e.getKey()));
-				Files.createDirectories(root.getParent());
-				Files.write(root, e.getValue().get());
+				String pathStr = String.join("/", e.getKey());
+				Path path = output.resolve(pathStr);
+				if(path.startsWith(output)) {
+					Files.createDirectories(path.getParent());
+					Files.write(path, e.getValue().get());
+				} else {
+					LOGGER.error("RRP contains out-of-directory path! \"" + pathStr + "\"");
+				}
 			}
 			
 			Path assets = output.resolve("assets");
@@ -546,11 +551,18 @@ public class RuntimeResourcePackImpl implements RuntimeResourcePack, ResourcePac
 	
 	private void write(Path dir, Identifier identifier, byte[] data) {
 		try {
-			Path file = dir.resolve(identifier.getNamespace()).resolve(identifier.getPath());
-			Files.createDirectories(file.getParent());
-			try(OutputStream output = Files.newOutputStream(file)) {
-				output.write(data);
+			String namespace = identifier.getNamespace();
+			String path = identifier.getPath();
+			Path file = dir.resolve(namespace).resolve(path);
+			if(file.startsWith(dir)) {
+				Files.createDirectories(file.getParent());
+				try(OutputStream output = Files.newOutputStream(file)) {
+					output.write(data);
+				}
+			} else {
+				LOGGER.error("RRP contains out-of-directory location! \"" + namespace + "/" + path + "\"");
 			}
+
 		} catch(IOException e) {
 			throw new RuntimeException(e);
 		}
